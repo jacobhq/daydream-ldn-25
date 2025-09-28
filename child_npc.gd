3c97078a2b1e0b29b7e27d72152165fb317d1686
Moyinoluwa Akinnawonu <moyinaki@gmail.com>
2025-09-28 17:20:05 +0100

    hi

--- child_npc.gd
+++ child_npc.gd
@@ -1,4 +1,5 @@
-extends Node2D
+#extends Node2D
+extends CharacterBody2D
 
 func _ready():
 	# Ensure the sprite receives mouse input
@@ -11,3 +12,4 @@ func _on_sprite_gui_input(event):
 	if event is InputEventMouseButton and event.pressed:
 		print("NPC clicked!")  # debug
 		get_tree().change_scene_to_file("res://DecisionScreen.tscn")
+		
