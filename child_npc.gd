95c0f0c1fad5e19956d01e7c5261854b3449117c
luxmannn <luxmanaravin@gmail.com>
2025-09-28 17:04:50 +0100

    hry

--- child_npc.gd
+++ child_npc.gd
@@ -1,2 +1,15 @@
+
 extends Node2D
-@onready var area: Area2D = $Area2D
+
+func _ready():
+	# Ensure the sprite receives mouse input
+	$Sprite2D.mouse_filter = Control.MOUSE_FILTER_PASS
+	# Connect the gui_input signal to a function on this node
+	$Sprite2D.connect("ui_accept", self, "_on_Sprite_gui_input")
+
+# This function is called when the sprite is clicked
+func _on_Sprite_gui_input(event):
+	if event is InputEventMouseButton and event.pressed:
+		print("NPC clicked!")
+		# Go to your DecisionScreen scene
+		get_tree().change_scene_to_file("res://DecisionScreen.tscn")
