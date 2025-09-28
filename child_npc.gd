59e3f740d42dad1332eb8154a247deada1aaa8ef
luxmannn <luxmanaravin@gmail.com>
2025-09-28 18:00:28 +0100

    hi

--- child_npc.gd
+++ child_npc.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+  
+
 @onready var area = $Area2D
 
 func _ready():
 	# connect signal to this instance
 	area.body_entered.connect(_on_body_entered)
+	add_to_group("player")
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
