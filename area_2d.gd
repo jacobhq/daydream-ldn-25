ea070b29bd84ce51e2e92f89b68cec080196eea5
luxmannn <luxmanaravin@gmail.com>
2025-09-28 17:55:53 +0100

    update

--- area_2d.gd
+++ area_2d.gd
@@ -1,13 +1,9 @@
-extends Node2D
-
-@onready var area = $Area2D
+extends Area2D
 
 func _ready():
-	# connect signal to this instance
-	area.body_entered.connect(_on_body_entered)
+	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		print("Player reached NPC!")
-		# Switch to your DecisionScreen scene
 		get_tree().change_scene_to_file("res://DecisionScreen.tscn")
