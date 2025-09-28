15ee1c7b1b34b19a74d354e58fd2f591d8dea1b2
luxmannn <luxmanaravin@gmail.com>
2025-09-28 17:20:45 +0100

    Update control.gd

--- control.gd
+++ control.gd
@@ -7,9 +7,9 @@ func _ready():
 func _on_sacrifice_pressed():
 	print("Player chose to sacrifice!")
 	# 
-	get_tree().change_scene_to_file("res://SacrificeOutcome.tscn")
+	get_tree().change_scene_to_file("res://prison.tscn")
 
 func _on_die_pressed():
 	print("Player chose to die!")
 
-	get_tree().change_scene_to_file("res://GameOver.tscn")
+	get_tree().change_scene_to_file("res://dungeon scene/dungeon.tscn")
