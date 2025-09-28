4c86ad9c5aa329629e7d771e20693c1e11709b0f
luxmannn <luxmanaravin@gmail.com>
2025-09-28 16:59:31 +0100

    Update child_npc.gd

--- child_npc.gd
+++ child_npc.gd
@@ -5,6 +5,6 @@ func _ready():
 	area.body_entered.connect(_on_child_npc_entered)
 	
 func _on_body_entered(body):
-	if body.is_in_group("player")
-	get_tree().change_scene_to_file()
+	if body.is_in_group("player"):
+		get_tree().change_scene_to_file()
 	
