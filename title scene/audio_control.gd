3b0ba4fac85b09ee85c71c754130cea1e68f7367
Jacob Marshall <me@jhqcat.com>
2025-09-28 16:32:13 +0100

    Sacrifice your ears

--- title scene/audio_control.gd
+++ title scene/audio_control.gd
@@ -1,5 +1,11 @@
 extends HSlider
 
+@export var audio_bus_name: String
+
+var audio_bus_id
+
+func _ready() -> void:
+	audio_bus_id = AudioServer.get_bus_index(audio_bus_name)
 
 func _on_value_changed(value: float) -> void:
-	print(value)
+	AudioServer.set_bus_volume_db(audio_bus_id, value)
